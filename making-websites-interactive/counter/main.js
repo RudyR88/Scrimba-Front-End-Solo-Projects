7f0f257d447dc7da4faab744fe338e111174ecd8
RudyRivera <combustingkarma@gmail.com>
2022-10-26 23:26:45 -0500

    counter project reset feature and syntax fix
    Added reset feature to project. Fixed missing semicolons

--- making-websites-interactive/counter/main.js
+++ making-websites-interactive/counter/main.js
@@ -1,24 +1,35 @@
+const countedEl = document.getElementById('counted');
+
 let count = 0;
 let savedValues = [];
-
-const countedEl = document.getElementById('counted');
 
 function renderCount() {
     countedEl.textContent = count;
 }
 
+function renderSavedEntrys(){
+    document.getElementById('saved-entrys').textContent = savedValues.join(" - ");
+}
+
 document.getElementsByClassName('increment-btn')[0].addEventListener('click', ()=>{
     count++;
     renderCount();
-})
+});
 
 document.getElementsByClassName('save-btn')[0].addEventListener('click', ()=>{
     if(count != 0){
         savedValues.push(count);
         countedEl.textContent = 0;
         count = 0;
-        document.getElementById('saved-entrys').textContent = savedValues.join(" - ");
+        renderSavedEntrys();
     }
-})
+});
 
-renderCount()
+document.getElementsByClassName('reset-btn')[0].addEventListener("click", ()=>{
+    count = 0;
+    savedValues = [];
+    renderCount();
+    renderSavedEntrys();
+});
+
+renderCount();
