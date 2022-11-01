c2c969e32b880cb36dcbfb4fe564afa4c99a8f1a
RudyRivera <combustingkarma@gmail.com>
2022-11-01 03:59:51 -0500

    Password generator new feature
    Added copy to clipboard feature to project

--- making-websites-interactive/password-generator/js/main.js
+++ making-websites-interactive/password-generator/js/main.js
@@ -2,11 +2,15 @@ import {characters, normalChars} from './data.js';
 
 const password1El = document.querySelector('#password1');
 const password2El = document.querySelector('#password2');
+const clipboardMessage = document.getElementsByClassName('msg')[1];
 
 document.querySelector('#generate-btn').addEventListener('click', generatePassword);
+document.getElementsByClassName('password-container')[0].addEventListener('click', ()=>{copyToClip(password1El)});
+document.getElementsByClassName('password-container')[1].addEventListener('click', ()=>{copyToClip(password2El)})
 
 function generatePassword(event){
     event.preventDefault();
+    clipboardMessage.textContent = "";
     const specialCharactersBool = document.querySelector('#special-chars').checked;
     const passwordLength = document.querySelector('#password-length').value;
     if(passwordLength < 15 || passwordLength > 100){
@@ -39,4 +43,13 @@ function getRandomPassword(length, data){
         password+=data[randomIndex];
     }
     return password;
+}
+
+function copyToClip(element){
+    clipboardMessage.textContent = "";
+    if(element.textContent){
+        const passwordNumber = element.id === "password1" ? "1" : "2";
+        clipboardMessage.textContent = `Password ${passwordNumber} copied to clipboad!`;
+        navigator.clipboard.writeText(element.textContent);
+    }
 }
